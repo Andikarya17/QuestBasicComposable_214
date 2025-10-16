e2a3fc11cc98c8d4063e1ba95b596299486e16f6
Andikarya17 <andikaryaputra17@gmail.com>
2025-10-16 12:27:15 +0700

    nambah notasibalok.png ke resource drawable

--- app/src/main/java/com/example/questbasiccomposable/ui/tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable/ui/tataletak.kt
@@ -111,51 +111,60 @@ fun TataLetakRowColumn(modifier: Modifier) {
 fun TataLetakBoxColumnRow(modifier: Modifier) {
     val gambar = painterResource(id = R.drawable.notasibalok)
     Column {
-        Column(
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "Col1_Row1_Komponen1")
-                Text(text = "Col1_Row1_Komponen2")
-                Text(text = "Col1_Row1_Komponen3")
-            }
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "Col1_Row2_Komponen1")
-                Text(text = "Col1_Row2_Komponen2")
-                Text(text = "Col1_Row2_Komponen3")
-            }
-
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-
         Box(
             modifier = modifier
                 .fillMaxWidth()
-                .height(300.dp)
-                .background(color = Color.Cyan),
+                .height(110.dp)
+                .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
         ) {
-            Image(
-                painter = gambar,
-                contentDescription = null,
-                contentScale = ContentScale.Fit
-            )
-            Text(
-                text = "My Music",
-                fontSize = 50.sp,
-                color = Color.Red,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Cursive,
-                modifier = Modifier.align(Alignment.Center)
-            )
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
+
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .background(color = Color.Cyan),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit
+                )
+                Text(
+                    text = "My Music",
+                    fontSize = 50.sp,
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+
+            }
 
         }
-
     }
 }
