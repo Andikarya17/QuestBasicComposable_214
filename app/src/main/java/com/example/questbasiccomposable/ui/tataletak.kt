c0acbb89741883b85959499b940f723ec960bb36
Andikarya17 <andikaryaputra17@gmail.com>
2025-10-16 11:56:37 +0700

    buat struktur awal fungsi TataLetakBoxColumnRow dan inisialisasi gambar notasibalok

--- app/src/main/java/com/example/questbasiccomposable/ui/tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable/ui/tataletak.kt
@@ -106,3 +106,10 @@ fun TataLetakRowColumn(modifier: Modifier) {
 
     }
 }
+
+@Composable
+fun TataLetakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.notasibalok)
+    Column {
+    }
+}
